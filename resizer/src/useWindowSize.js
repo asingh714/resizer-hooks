7a06ea61873f64ac919085963a822889a2781f12
Aman <amsingh714@gmail.com>
2019-03-31 12:59:46 -0400

    app is fully responsive to window resizing

--- resizer/src/useWindowSize.js
+++ resizer/src/useWindowSize.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export default function useWindowSize() {
-  const isClient = typeof window === 'object';
+export default function useWindowSize(searchTerm) {
+  const isClient = typeof window === "object";
+  const [windowSize, setWindowSize] = useState(getSize);
 
   function getSize() {
     return {
@@ -10,5 +11,22 @@ export default function useWindowSize() {
     };
   }
 
-  return getSize();
-} 
+  // set up a listener - componentDidMount, cleaned up in componentWillUnmount
+  useEffect(() => {
+    if (!isClient) return false;
+    console.log("adding event listener");
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // will only run the cb onMount and willUnmount
+
+  useEffect(() => {
+    // make an API call
+  }, [searchTerm]); // will run on mount and when searchTerm changes
+
+  return windowSize;
+}
